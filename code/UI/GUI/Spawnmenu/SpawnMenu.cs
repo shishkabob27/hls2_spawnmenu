add7cf5ed5d279cecc4ff841c44a09c7ca582525
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2022-10-29 13:21:03 -0500

    cleanup

--- code/UI/GUI/Spawnmenu/SpawnMenu.cs
+++ code/UI/GUI/Spawnmenu/SpawnMenu.cs
@@ -6,46 +6,16 @@ global using Sandbox.UI;
 global using Sandbox.UI.Tests;
 global using Sandbox.UI.Construct;
 namespace SpawnMenu;
-[Library]
-[UseTemplate("/resource/templates/SpawnMenu.html")]
+[Library, UseTemplate("/resource/templates/SpawnMenu.html")]
 public partial class SpawnMenu : GUIPanel
 {
-	public static SpawnMenu Instance;
-
 	public SpawnMenu()
 	{
-		Instance = this;
-
-		//StyleSheet.Load("/ui/spawnmenu.scss");
-
         Style.Left = 0;
         Style.Right = 0;
         Style.Top = 0;
         Style.Bottom = 0;
         Focus();
-
-        /*var left = Add.Panel( "left" );
-		{
-			var tabs = left.AddChild<ButtonGroup>();
-			tabs.AddClass( "tabs" );
-
-			var body = left.Add.Panel( "body" );
-
-			{
-				var npc = body.AddChild<NPCList>();
-				tabs.SelectedButton = tabs.AddButtonActive( "NPC", ( b ) => npc.SetClass( "active", b ) );
-
-				var weapon = body.AddChild<WeaponList>();
-				tabs.AddButtonActive( "Weapons", ( b ) => weapon.SetClass( "active", b ) );
-
-				var ents = body.AddChild<EntityList>();
-				tabs.AddButtonActive( "Entities", ( b ) => ents.SetClass( "active", b ) );
-
-				var models = body.AddChild<CloudModelList>();
-				tabs.AddButtonActive( "asset.party", ( b ) => models.SetClass( "active", b ) );
-			}
-		};*/
-
 	}
 
     public override void Tick()
@@ -67,12 +37,9 @@ public partial class SpawnMenu : GUIPanel
     }
 }
 
-
 partial class HLGame
 {
-
 	static SMhudpanel sphudpanel { get; set; }
-
 
 	[ConCmd.Client]
 	public static void create_spawnmenu()
@@ -84,9 +51,6 @@ partial class HLGame
 
 public class SMhudpanel : HudEntity<GUIRootPanel>
 {
-    public static SMhudpanel Current;
-
-
     public SMhudpanel()
     {
             RootPanel.AddChild<SpawnMenu>();
