6011bb0f0dbcb241049d8ca20100b64c54adf4bd
Xenthio <thegooster88@gmail.com>
2023-04-14 11:11:09 +1000

    more pain day fixes

--- code/UI/GUI/Spawnmenu/left/CloudModelList.cs
+++ code/UI/GUI/Spawnmenu/left/CloudModelList.cs
@@ -1,4 +1,4 @@
-﻿using Sandbox;
+﻿using Sandbox.UI.Tests;
 
 namespace SpawnMenuAddon;
 
@@ -9,105 +9,105 @@ public partial class CloudModelList : Panel
 
 	public CloudModelList()
 	{
-        AddClass("spawnpage");
-        AddChild(out Canvas, "canvas");
+		AddClass("spawnpage");
+		AddChild(out Canvas, "canvas");
 
-        Canvas.Layout.AutoColumns = true;
-        Canvas.Layout.ItemWidth = 91;
-        Canvas.Layout.ItemHeight = 91;
+		Canvas.Layout.AutoColumns = true;
+		Canvas.Layout.ItemWidth = 91;
+		Canvas.Layout.ItemHeight = 91;
 
-        Canvas.OnCreateCell = (cell, data) =>
-        {
-            var file = (Package)data;
-            var btn = cell.Add.Button(file.Title);
-            btn.AddClass("icon");
-            btn.AddEventListener("onclick", () => ConsoleSystem.Run( "spawnmenu_spawn", file.FullIdent, SpawnMenu.Current.TypeSelector.ActiveTab) );
-            btn.Style.BackgroundImage = Texture.Load(file.Thumb);
-        };
+		Canvas.OnCreateCell = (cell, data) =>
+		{
+			var file = (Package)data;
+			var btn = cell.Add.Button(file.Title);
+			btn.AddClass("icon");
+			btn.AddEventListener("onclick", () => ConsoleSystem.Run("spawnmenu_spawn", file.FullIdent, SpawnMenu.Current.TypeSelector.ActiveTab));
+			btn.Style.BackgroundImage = Texture.Load(file.Thumb);
+		};
 
-        _ = UpdateItems();
-    }
+		_ = UpdateItems();
+	}
 	string prevSearch = "";
-    string prevtab;
-    bool prevshowincomp;
-    public override void Tick()
-    {
-        if ( SpawnMenu.Current.MainSelector.ActiveTab != "assetparty" ) return;
-        if ( prevSearch == SpawnMenu.Current.SearchQuery && prevtab == SpawnMenu.Current.SelectedTab && prevshowincomp == SpawnMenu.Current.ShowIncompatible ) return;
+	string prevtab;
+	bool prevshowincomp;
+	public override void Tick()
+	{
+		if (SpawnMenu.Current.MainSelector.ActiveTab != "assetparty") return;
+		if (prevSearch == SpawnMenu.Current.SearchQuery && prevtab == SpawnMenu.Current.SelectedTab && prevshowincomp == SpawnMenu.Current.ShowIncompatible) return;
 		prevSearch = SpawnMenu.Current.SearchQuery;
 		prevtab = SpawnMenu.Current.SelectedTab;
 		prevshowincomp = SpawnMenu.Current.ShowIncompatible;
-        RefreshItems();
-	} 
-	public async Task UpdateItems( int offset = 0 )
+		RefreshItems();
+	}
+	public async Task UpdateItems(int offset = 0)
 	{
-		var q = new Package.Query();
-		q.Type = Package.Type.Model;
-		 
-        switch ( SpawnMenu.Current.TypeSelector.ActiveTab )
-        {
-            case "model":
-                q.Type = Package.Type.Model;
-                break;
-            case "addon":
-                q.Type = Package.Type.Addon;
-                break;
-            case "material":
-                q.Type = Package.Type.Material;
-                break;
-            case "map":
-                q.Type = Package.Type.Map;
-                break;
-            case "sound":
-                q.Type = Package.Type.Sound;
-                break;
-        }
+		var Type = "type:model";
 
-        q.Order = Package.Order.Newest;
-		switch(prevtab)
+		switch (SpawnMenu.Current.TypeSelector.ActiveTab)
+		{
+			case "model":
+				Type = "type:model";
+				break;
+			case "addon":
+				Type = "type:addon";
+				break;
+			case "material":
+				Type = "type:material";
+				break;
+			case "map":
+				Type = "type:map";
+				break;
+			case "sound":
+				Type = "type:sound";
+				break;
+		}
+
+
+		var Order = "order:newest";
+		switch (prevtab)
 		{
 			case "Most Recent":
-                q.Order = Package.Order.Newest;
+				Order = "order:newest";
 				break;
 			case "Most Popular":
-                q.Order = Package.Order.Popular;
+				Order = "order:popular";
 				break;
 			case "Most Downloads":
-                q.Order = Package.Order.Live;
+				Order = "order:live";
 				break;
 			case "Trending":
-                q.Order = Package.Order.Trending;
+				Order = "order:trending";
 				break;
-        }
-		q.Take = 200;
-		q.Skip = offset;
-		q.SearchText = SpawnMenu.Current.SearchQuery;
+		}
+		var Take = 200;
+		var Skip = offset;
+		var Search = "search:" + SpawnMenu.Current.SearchQuery;
 
-		var found = await q.RunAsync( default );
-		var foundNew = found.ToList();
-		if ( SpawnMenu.Current.TypeSelector.ActiveTab == "addon" && !SpawnMenu.Current.ShowIncompatible)
-        {
-			foundNew.RemoveAll( x => !( x.GetMeta<string>( "ParentPackage" ) == "shishkabob.hls2" || x.Tags.Contains("game:any") || x.Tags.Contains( "game:shishkabob.hls2" ) ) ) ; 
-        }
-		Canvas.SetItems( foundNew );
+		var found = await Package.FindAsync($"{Order} {Type} {Search}", Take, Skip);
+		var foundNew = found.Packages.ToList();
+		if (SpawnMenu.Current.TypeSelector.ActiveTab == "addon" && !SpawnMenu.Current.ShowIncompatible)
+		{
+			foundNew.RemoveAll(x => !(x.GetMeta<string>("ParentPackage") == "shishkabob.hls2" || x.Tags.Contains("game:any") || x.Tags.Contains("game:shishkabob.hls2")));
+		}
+		Canvas.SetItems(foundNew);
 
-		for (var i = 1; i < 5; i++ )
-        {
-            q.Take = 200;
-            q.Skip = 200 * i;
-            q.SearchText = SpawnMenu.Current.SearchQuery;
+		for (var i = 1; i < 5; i++)
+		{
+			Take = 200;
+			Skip = 200 * i;
+			Search = "search:" + SpawnMenu.Current.SearchQuery;
 
-            var found2 = await q.RunAsync( default );
-            var foundNew2 = found2.ToList();
-            if ( SpawnMenu.Current.TypeSelector.ActiveTab == "addon" && !SpawnMenu.Current.ShowIncompatible )
-            {
-                foundNew2.RemoveAll( x => !( x.GetMeta<string>( "ParentPackage" ) == "shishkabob.hls2" || x.Tags.Contains( "game:any" ) || x.Tags.Contains( "game:shishkabob.hls2" ) ) );
-            }
-            Canvas.AddItems( foundNew2.ToArray() );
-        }
+			var found2 = await Package.FindAsync($"{Order} {Type} {Search}", Take, Skip);
+			var foundNew2 = found2.Packages.ToList();
+			if (SpawnMenu.Current.TypeSelector.ActiveTab == "addon" && !SpawnMenu.Current.ShowIncompatible)
+			{
+				foundNew2.RemoveAll(x => !(x.GetMeta<string>("ParentPackage") == "shishkabob.hls2" || x.Tags.Contains("game:any") || x.Tags.Contains("game:shishkabob.hls2")));
+			}
+			Canvas.AddItems(foundNew2.ToArray());
+		}
 
-        // TODO - auto add more items here
-    }
+		// TODO - auto add more items here
+	}
 
 	public void RefreshItems()
 	{
@@ -115,160 +115,162 @@ public partial class CloudModelList : Panel
 		_ = UpdateItems();
 	}
 
-	static async Task<string> SpawnPackageModel( string packageName, Vector3 pos, Rotation rotation, Entity source )
+	static async Task<string> SpawnPackageModel(string packageName, Vector3 pos, Rotation rotation, Entity source)
 	{
-		var package = await Package.Fetch( packageName, false );
-		if ( package == null || package.PackageType != Package.Type.Model || package.Revision == null )
+		var package = await Package.Fetch(packageName, false);
+		if (package == null || package.PackageType != Package.Type.Model || package.Revision == null)
 		{
 			// spawn error particles
 			return null;
 		}
 
-		if ( !source.IsValid ) return null; // source entity died or disconnected or something
+		if (!source.IsValid) return null; // source entity died or disconnected or something
 
-		var model = package.GetMeta( "PrimaryAsset", "models/dev/error.vmdl" );
-		var mins = package.GetMeta( "RenderMins", Vector3.Zero );
-		var maxs = package.GetMeta( "RenderMaxs", Vector3.Zero );
+		var model = package.GetMeta("PrimaryAsset", "models/dev/error.vmdl");
+		var mins = package.GetMeta("RenderMins", Vector3.Zero);
+		var maxs = package.GetMeta("RenderMaxs", Vector3.Zero);
 
 		// downloads if not downloads, mounts if not mounted
 		await package.MountAsync();
 
 		return model;
-	} 
-	static async Task<string> SpawnPackageSound( string packageName, Vector3 pos, Rotation rotation, Entity source )
+	}
+	static async Task<string> SpawnPackageSound(string packageName, Vector3 pos, Rotation rotation, Entity source)
 	{
-		var package = await Package.Fetch( packageName, false );
-		if ( package == null || package.PackageType != Package.Type.Sound || package.Revision == null )
+		var package = await Package.Fetch(packageName, false);
+		if (package == null || package.PackageType != Package.Type.Sound || package.Revision == null)
 		{
 			// spawn error particles
 			return null;
 		}
 
-		if ( !source.IsValid ) return null; // source entity died or disconnected or something
+		if (!source.IsValid) return null; // source entity died or disconnected or something
 
-		var asset = package.GetMeta( "PrimaryAsset", "sounds/dev/error.sound" );
+		var asset = package.GetMeta("PrimaryAsset", "sounds/dev/error.sound");
 
 		// downloads if not downloads, mounts if not mounted
-		await package.MountAsync();   
-        Log.Info( $"Playing sound {asset}" ); 
-        Sound.FromEntity( asset, source );
-        return asset;
+		await package.MountAsync();
+		Log.Info($"Playing sound {asset}");
+		Sound.FromEntity(asset, source);
+		return asset;
 
-    }		
-	static async Task<string> SpawnPackageMaterial( string packageName, Entity source )
+	}
+	static async Task<string> SpawnPackageMaterial(string packageName, Entity source)
 	{
-		var package = await Package.Fetch( packageName, false );
-		if ( package == null || package.PackageType != Package.Type.Material || package.Revision == null )
+		var package = await Package.Fetch(packageName, false);
+		if (package == null || package.PackageType != Package.Type.Material || package.Revision == null)
 		{
 			// spawn error particles
 			return null;
 		}
 
-		if ( !source.IsValid ) return null; // source entity died or disconnected or something
+		if (!source.IsValid) return null; // source entity died or disconnected or something
 
-		var asset = package.GetMeta( "PrimaryAsset", "materials/dev/error.vmat" );
+		var asset = package.GetMeta("PrimaryAsset", "materials/dev/error.vmat");
 
 		// downloads if not downloads, mounts if not mounted
-		await package.MountAsync();   
-        return asset;
+		await package.MountAsync();
+		return asset;
 
-    }	
+	}
 
-	static async Task<string> SpawnPackageAddon( string packageName, Entity source )
+	static async Task<string> SpawnPackageAddon(string packageName, Entity source)
 	{
-		var package = await Package.Fetch( packageName, false );
-		if ( package == null || package.PackageType != Package.Type.Addon || package.Revision == null )
+		var package = await Package.Fetch(packageName, false);
+		if (package == null || package.PackageType != Package.Type.Addon || package.Revision == null)
 		{
 			// spawn error particles
 			return null;
 		}
 
-		if ( !source.IsValid ) return null; // source entity died or disconnected or something
+		if (!source.IsValid) return null; // source entity died or disconnected or something
 
-		var asset = package.GetMeta( "PrimaryType", "ErrorType" );
-		var parentpkg = package.GetMeta( "ParentPackage", "all games." );
+		var asset = package.GetMeta("PrimaryType", "ErrorType");
+		var parentpkg = package.GetMeta("ParentPackage", "all games.");
 
 		// downloads if not downloads, mounts if not mounted
 		await package.MountAsync(true);
-		
-        Log.Info( $"Loading addon \"{packageName}\" for \"{parentpkg}\" with primary type \"{asset}\"" );  
-        return asset;
 
-    }	
-	static async Task<string> SpawnPackageGeneric( string packageName, Entity source )
+		Log.Info($"Loading addon \"{packageName}\" for \"{parentpkg}\" with primary type \"{asset}\"");
+		return asset;
+
+	}
+	static async Task<string> SpawnPackageGeneric(string packageName, Entity source)
 	{
-		var package = await Package.Fetch( packageName, false );
-		if ( package == null || package.Revision == null )
+		var package = await Package.Fetch(packageName, false);
+		if (package == null || package.Revision == null)
 		{
 			// spawn error particles
 			return null;
 		}
 
-		if ( !source.IsValid ) return null; // source entity died or disconnected or something
+		if (!source.IsValid) return null; // source entity died or disconnected or something
 
-		var asset = package.GetMeta( "PrimaryAsset", "sounds/dev/error" );
+		var asset = package.GetMeta("PrimaryAsset", "sounds/dev/error");
 
 		// downloads if not downloads, mounts if not mounted
-		await package.MountAsync();   
-		
-        Log.Info( $"Loading package {asset}" );  
-        return asset;
+		await package.MountAsync();
 
-    }
+		Log.Info($"Loading package {asset}");
+		return asset;
 
-	[ConCmd.Server( "spawnmenu_spawn" )]
-	public static void Spawn( string ident , string activetab )
+	}
+
+	[ConCmd.Server("spawnmenu_spawn")]
+	public static void Spawn(string ident, string activetab)
 	{
-		var owner = ConsoleSystem.Caller?.Pawn;
-		Log.Info( ident ); 
-		if ( ConsoleSystem.Caller == null )
+		var owner = ConsoleSystem.Caller?.Pawn as Entity;
+		Log.Info(ident);
+		if (ConsoleSystem.Caller == null)
 			return;
-        switch ( activetab )
-        {
-            case "model":
-				SpawnModel( ident, owner );
-                break;
-            case "addon":
-                DownloadAddon( ident, owner );
-                break;
-            case "material":
-                SetMaterial( ident, owner );
-                break;
-            case "map":
-				Global.ChangeLevel( ident );
-                break;
+		switch (activetab)
+		{
+			case "model":
+				SpawnModel(ident, owner);
+				break;
+			case "addon":
+				DownloadAddon(ident, owner);
+				break;
+			case "material":
+				SetMaterial(ident, owner);
+				break;
+			case "map":
+				Game.ChangeLevel(ident);
+				break;
 			case "sound":
-                SpawnPackageSound( ident, Vector3.Zero, Rotation.From( Angles.Zero ), owner );
-                break;
+				SpawnPackageSound(ident, Vector3.Zero, Rotation.From(Angles.Zero), owner);
+				break;
 			default:
-                SpawnModel( ident, owner );
-                break;
-        }
-    }
-	static async void DownloadAddon( string ident, Entity owner ) {
-		var modelname = await SpawnPackageAddon( ident, owner );
-    }
-	static async void SpawnModel( string modelname, Entity owner ) {
+				SpawnModel(ident, owner);
+				break;
+		}
+	}
+	static async void DownloadAddon(string ident, Entity owner)
+	{
+		var modelname = await SpawnPackageAddon(ident, owner);
+	}
+	static async void SpawnModel(string modelname, Entity owner)
+	{
 
-        var tr = Trace.Ray( owner.EyePosition, owner.EyePosition + owner.EyeRotation.Forward * 500 )
-            .UseHitboxes()
-            .Ignore( owner )
-            .Run();
+		var tr = Trace.Ray(owner.AimRay.Position, owner.AimRay.Position + owner.AimRay.Forward * 500)
+			.UseHitboxes()
+			.Ignore(owner)
+			.Run();
 
-        var modelRotation = Rotation.From( new Angles( 0, owner.EyeRotation.Angles().yaw, 0 ) ) * Rotation.FromAxis( Vector3.Up, 180 );
+		var modelRotation = Rotation.From(new Angles(0, owner.AimRay.Forward.EulerAngles.yaw, 0)) * Rotation.FromAxis(Vector3.Up, 180);
 
 		//
 		// Does this look like a package?
 		//
-		if ( modelname.Count( x => x == '.' ) == 1 && !modelname.EndsWith( ".vmdl", System.StringComparison.OrdinalIgnoreCase ) && !modelname.EndsWith( ".vmdl_c", System.StringComparison.OrdinalIgnoreCase ) )
+		if (modelname.Count(x => x == '.') == 1 && !modelname.EndsWith(".vmdl", System.StringComparison.OrdinalIgnoreCase) && !modelname.EndsWith(".vmdl_c", System.StringComparison.OrdinalIgnoreCase))
 		{
-			modelname = await SpawnPackageModel( modelname, tr.EndPosition, modelRotation, owner );
-			if ( modelname == null )
+			modelname = await SpawnPackageModel(modelname, tr.EndPosition, modelRotation, owner);
+			if (modelname == null)
 				return;
 		}
 
-		var model = Model.Load( modelname );
-		if ( model == null || model.IsError )
+		var model = Model.Load(modelname);
+		if (model == null || model.IsError)
 			return;
 
 		var ent = new Prop
@@ -279,40 +281,41 @@ public partial class CloudModelList : Panel
 		};
 
 		// Let's make sure physics are ready to go instead of waiting
-		ent.SetupPhysicsFromModel( PhysicsMotionType.Dynamic );
+		ent.SetupPhysicsFromModel(PhysicsMotionType.Dynamic);
 
 		// If there's no physics model, create a simple OBB
-		if ( !ent.PhysicsBody.IsValid() )
+		if (!ent.PhysicsBody.IsValid())
 		{
-			ent.SetupPhysicsFromOBB( PhysicsMotionType.Dynamic, ent.CollisionBounds.Mins, ent.CollisionBounds.Maxs );
+			ent.SetupPhysicsFromOBB(PhysicsMotionType.Dynamic, ent.CollisionBounds.Mins, ent.CollisionBounds.Maxs);
 		}
 	}
-	static async void SetMaterial( string matname, Entity owner ) {
+	static async void SetMaterial(string matname, Entity owner)
+	{
 
-        var tr = Trace.Ray( owner.EyePosition, owner.EyePosition + owner.EyeRotation.Forward * 500 )
-            .UseHitboxes()
-            .Ignore( owner )
-            .Run();
+		var tr = Trace.Ray(owner.AimRay.Position, owner.AimRay.Position + owner.AimRay.Forward * 500)
+			.UseHitboxes()
+			.Ignore(owner)
+			.Run();
 
 		//
 		// Does this look like a package?
 		//
-		if ( matname.Count( x => x == '.' ) == 1 && !matname.EndsWith( ".vmat", System.StringComparison.OrdinalIgnoreCase ) && !matname.EndsWith( ".vmat_c", System.StringComparison.OrdinalIgnoreCase ) )
+		if (matname.Count(x => x == '.') == 1 && !matname.EndsWith(".vmat", System.StringComparison.OrdinalIgnoreCase) && !matname.EndsWith(".vmat_c", System.StringComparison.OrdinalIgnoreCase))
 		{
-            matname = await SpawnPackageMaterial( matname, owner );
-			if ( matname == null )
+			matname = await SpawnPackageMaterial(matname, owner);
+			if (matname == null)
 				return;
 		}
 
-        Log.Info( $"Setting material {matname}" );
+		Log.Info($"Setting material {matname}");
 
-        var material = Material.Load( matname );
+		var material = Material.Load(matname);
 		//if ( material == null || material.IsPromise )
-			//return;
+		//return;
 
 		if (tr.Entity is ModelEntity mdlent)
 		{
-			mdlent.SetMaterialOverride( material );
+			mdlent.SetMaterialOverride(material);
 		}
 	}
 }
--- code/UI/GUI/Spawnmenu/left/WeaponList.cs
+++ code/UI/GUI/Spawnmenu/left/WeaponList.cs
@@ -1,6 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using Sandbox.UI.Tests;
 namespace SpawnMenuAddon;
 
 [Library]
@@ -9,70 +7,71 @@ public partial class WeaponList : Panel
 	VirtualScrollPanel Canvas;
 	public WeaponList()
 	{
-		AddClass( "spawnpage" );
-		AddChild( out Canvas, "canvas" );
+		AddClass("spawnpage");
+		AddChild(out Canvas, "canvas");
 
 		Canvas.Layout.AutoColumns = true;
-        Canvas.Layout.ItemWidth = 92;
-        Canvas.Layout.ItemHeight = 92;
-        Canvas.OnCreateCell = ( cell, data ) =>
+		Canvas.Layout.ItemWidth = 92;
+		Canvas.Layout.ItemHeight = 92;
+		Canvas.OnCreateCell = (cell, data) =>
 		{
-			if ( data is TypeDescription type )
+			if (data is TypeDescription type)
 			{
-				var btn = cell.Add.Button( type.Title );
-				btn.AddClass( "icon" );
-				btn.AddEventListener( "onclick", () => ConsoleSystem.Run( "ent_create", type.ClassName ) );
-				var t = Texture.Load( FileSystem.Mounted, $"/ui/spawnmenu/{type.ClassName}.png", false );
-				if (t == null || t.IsPromise || SpawnMenu.AlwaysUseRenderedIcons )
+				var btn = cell.Add.Button(type.Title);
+				btn.AddClass("icon");
+				btn.AddEventListener("onclick", () => ConsoleSystem.Run("ent_create", type.ClassName));
+				var t = Texture.Load(FileSystem.Mounted, $"/ui/spawnmenu/{type.ClassName}.png", false);
+				if (t == null || t.IsPromise || SpawnMenu.AlwaysUseRenderedIcons)
 				{
-					btn.Style.BackgroundImage = SpawnMenu.MakeIcon( type );
-                } else
-                {
-                    btn.Style.BackgroundImage = t;
-                    btn.Style.BackgroundPositionX = Length.Auto;
-                    btn.Style.BackgroundPositionY = Length.Auto;
-                }
+					btn.Style.BackgroundImage = SpawnMenu.MakeIcon(type);
+				}
+				else
+				{
+					btn.Style.BackgroundImage = t;
+					btn.Style.BackgroundPositionX = Length.Auto;
+					btn.Style.BackgroundPositionY = Length.Auto;
+				}
 				var x = type.GetType();
 
-               
+
 			}
 		};
 
 
 
-    }
-	string[] HL1Weapons = new string[] { "weapon_crowbar", "weapon_9mmhandgun", "weapon_357", "weapon_mp5", "weapon_shotgun", "weapon_crossbow", "weapon_rpg", "weapon_gauss", "weapon_egon", "weapon_hornet","weapon_handgrenade","weapon_tripmine","weapon_satchel","weapon_snark" };
-    string prevtab;
-    [Event.Tick]
-    void update()
-    {
-        if ( prevtab == SpawnMenu.Current.SelectedTab ) return;
-        prevtab = SpawnMenu.Current.SelectedTab;
-        if ( SpawnMenu.Current.MainSelector.ActiveTab != "weapons" ) return;
+	}
+	string[] HL1Weapons = new string[] { "weapon_crowbar", "weapon_9mmhandgun", "weapon_357", "weapon_mp5", "weapon_shotgun", "weapon_crossbow", "weapon_rpg", "weapon_gauss", "weapon_egon", "weapon_hornet", "weapon_handgrenade", "weapon_tripmine", "weapon_satchel", "weapon_snark" };
+	string prevtab;
+	[Event.Tick]
+	void update()
+	{
+		if (prevtab == SpawnMenu.Current.SelectedTab) return;
+		prevtab = SpawnMenu.Current.SelectedTab;
+		if (SpawnMenu.Current.MainSelector.ActiveTab != "weapons") return;
 
-        Canvas.Clear();
-        //GARBAGE
+		Canvas.Clear();
+		//GARBAGE
 
 
 
-        var ents = TypeLibrary.GetDescriptions<Weapon>()
-                                    .OrderBy( x => x.Title )
-                                    .ToArray();
+		var ents = TypeLibrary.GetTypes<Weapon>()
+									.OrderBy(x => x.Title)
+									.ToArray();
 
-        foreach ( var entry in ents )
-        {
-            string a = "Other";
-            try
-            {
-                a = (string)entry.GetAttribute<MenuCategory>().Value;
-            }
-            catch { }
-            SpawnMenu.CheckCategory( a );
-            if ( a == SpawnMenu.Current.SelectedTab )
-            {
-                Canvas.AddItem( entry );
-            }
+		foreach (var entry in ents)
+		{
+			string a = "Other";
+			try
+			{
+				a = (string)entry.GetAttribute<MenuCategory>().Value;
+			}
+			catch { }
+			SpawnMenu.CheckCategory(a);
+			if (a == SpawnMenu.Current.SelectedTab)
+			{
+				Canvas.AddItem(entry);
+			}
 
-        }
-    }
-} 
+		}
+	}
+}
